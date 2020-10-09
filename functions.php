7b94994f6db1c79646c833c52adffdd26bd67b00
killianclaeyssen <killian.claeyssen@gmail.com>
2020-10-09 15:59:06 +0200

    modif

--- functions.php
+++ functions.php
@@ -1,6 +1,10 @@
 //ajout du titre
+<?php>
 function ajout_title(){
  add_theme_support("title_tag");
-
+ //ajout du style
+ function ajout_style('general',get_template_directory_uri()!css(style.css));
+ // wp_enque_script ('special', get_template_directory_uri.'js/script.js')
 }
+add_action ('wp_enqueue_scripts','ajout_style');
 add_action("after_setup_theme","ajout_titre");
