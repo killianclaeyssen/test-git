162817d14d1ccdc78e75f35d0732fc216367f987
killianclaeyssen <killian.claeyssen@gmail.com>
2020-11-13 15:32:12 +0100

    modification de la home page

--- home.php
+++ home.php
@@ -1,31 +1,20 @@
 
-<?php
- get_header() ;
-  ?>
-<section>
+<?php get_header(); ?>
+        <section>
 
-
-	<h2> Bonjour </h2>
+    <p>les pierres</p>
+<!-- The Loop (la boucle)-->
+<?php if (have_posts()):
+    while (have_posts()):
+        the_post();?>
+        <article>
+            <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+            <p><?php the_time();?></p>
+            <p><?php the_author_posts_link();?></p>
+            <div><?php the_content();?></div>
+            <p><?php comments_number();?></p>
+        </article>
+        <?php endwhile; else : ?> <p>Désolé, bitch</p>
+    <?php endif;?>
 </section>
-
-
-
-<?php get_footer();?>
-<!-- the loop (la boucle)-->
-<!—The Loop (la boucle) -->
-<?php if( have_posts() ) :
- 	while ( have_posts() ) :
-  				 the_post(); ?>
-   					<article>
-   						<p> <?php the_time();?> </p>
-   						<p> <?php the_author_posts_link();?> </p>
-   						<h2>
-   						<a href="<?php the_permalink(); ?>">
-                <?php the_title();?>
-              </a>
-   						</h2>
-   							<div>
-   								<?php the_content(); ?>
-   							</div>
-   					 </article>
-   		 <?php endwhile; endif; ?>
+    <?php get_footer(); ?>
